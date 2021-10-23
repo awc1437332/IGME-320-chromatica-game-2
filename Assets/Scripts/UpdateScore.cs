12ad5237d507148ec6e11d076b07d624a6c297e4
juuu-jiii <52075702+juuu-jiii@users.noreply.github.com>
2021-10-23 18:07:55 -0400

    Jumps now increment progress. Scoring functionality completed.

---
+++ Assets/Scripts/UpdateScore.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateScore : MonoBehaviour
+{
+    [SerializeField]
+    private Text score;
+
+    [SerializeField]
+    private GameObject player;
+    private Player playerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScript = player.GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = playerScript.Score.ToString("D7");
+    }
+}
